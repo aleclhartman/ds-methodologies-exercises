ebdf9f97cdcacacb49216dbca58dfeabc7a01513
Alec Hartman <aleclhartman08@gmail.com>
2020-03-31 10:07:03 -0500

    reorganized

--- regression/wrangle.py
+++ regression/wrangle.py
@@ -3,13 +3,13 @@ import pandas as pd
 
 from env import get_db_url
 
-telco_url = get_db_url("telco_churn")
-
 telco_query = """
 SELECT c.customer_id, c.monthly_charges, c.tenure, c.total_charges
 FROM customers AS c
 JOIN contract_types AS ct USING(contract_type_id)
 WHERE ct.contract_type = 'Two year'"""
+
+telco_url = get_db_url("telco_churn")
 
 def wrangle_telco():
     customers = pd.read_sql(telco_query, telco_url)
