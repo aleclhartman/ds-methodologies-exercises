398fbfcdffb8c8afac73969c9d9303763e8c3856
Alec Hartman <aleclhartman08@gmail.com>
2020-04-06 14:57:49 -0500

    continued optimization of eda functions

--- regression/explore.py
+++ regression/explore.py
@@ -4,43 +4,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def plot_variable_pairs(dataframe):
-    sns.pairplot(dataframe, kind="reg")
+def plot_variable_pairs(dataframe, hue=None):
+    g = sns.pairplot(dataframe, hue=hue, kind="reg")
+    g.fig.suptitle("Scatterplot with Regression for Continuous Variables")
+    plt.show()
 
 def months_to_years(tenure_months, df, rounding=False):
     if rounding:
         df["tenure_years"] = np.round(tenure_months / 12)
+        return df
     else:
         df["tenure_years"] = np.round(tenure_months // 12)
-    return df
+        return df
 
-# make this function more dynamic
 def plot_categorical_and_continuous_vars(categorical_var, continuous_var, df):
     plt.figure(figsize=(32, 18))
-    
     plt.subplot(221)
-    barplot = sns.barplot(x=categorical_var, y=continuous_var, data=df, palette=sns.color_palette("colorblind"))
-    plt.xlabel("Years")
-    plt.ylabel("Dollars")
-    plt.title("Total Charges in Dollars by Tenure in Years")
-    plt.show()
-    
+    sns.barplot(x=categorical_var, y=continuous_var, data=df, palette=sns.color_palette("colorblind"))
     plt.figure(figsize=(32, 18))
-    
     plt.subplot(222)
-    boxplot = sns.boxplot(x=categorical_var, y=continuous_var, data=df, palette=sns.color_palette("colorblind"))
-    plt.xlabel("Years")
-    plt.ylabel("Dollars")
-    plt.title("Total Charges in Dollars by Tenure in Years")
-    plt.show()
-    
+    sns.boxplot(x=categorical_var, y=continuous_var, data=df, palette=sns.color_palette("colorblind"))
     plt.figure(figsize=(32, 18))
-    
     plt.subplot(223)
-    swarmplot = sns.swarmplot(x=categorical_var, y=continuous_var, data=df, palette=sns.color_palette("colorblind"))
-    plt.xlabel("Years")
-    plt.ylabel("Dollars")
-    plt.title("Total Charges in Dollars by Tenure in Years")
+    sns.swarmplot(x=categorical_var, y=continuous_var, data=df, palette=sns.color_palette("colorblind"))
     plt.show()
-
-    return barplot, boxplot, swarmplot
