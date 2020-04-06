ff621f935526af1447327b6209899778d8c5d4cf
Alec Hartman <aleclhartman08@gmail.com>
2020-04-06 11:25:07 -0500

    copied functions from jupyter notebook to script

--- regression/feature_engineering.py
+++ regression/feature_engineering.py
@@ -9,14 +9,19 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.linear_model import LinearRegression
 
-
-
-
 def select_kbest(features, target_variable, k):
-    """
-    This function takes in a pandas DataFrame, a target variable, and k as the amount of features to return according to the Select K Best class. 
-    """
-    X2 = SelectKBest(f_regression, k=k).fit_transform(features, target_variable)
+    f_selector = SelectKBest(f_regression, k=k)
+    f_selector.fit(features, target_variable)
+    X_reduced = f_selector.transform(features)
     f_support = f_selector.get_support()
-    f_feature = X_train.loc[:, f_support].columns.tolist()
+    f_feature = features.loc[:,f_support].columns.tolist()
     return f_feature
+
+def rfe(features, target_variable, k):
+    lm = LinearRegression()
+    rfe = RFE(lm, k)
+    rfe.fit(features, target_variable)
+    X_rfe = rfe.transform(features)
+    mask = rfe.support_
+    rfe_features = features.loc[:, mask].columns.tolist()
+    return rfe_features
