a52ae68233fda46ede1f1f74547640f074b32727
Alec Hartman <aleclhartman08@gmail.com>
2020-04-06 17:29:23 -0500

    committing satisfactory changes to feature engineering functions

--- regression/feature_engineering.py
+++ regression/feature_engineering.py
@@ -4,24 +4,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+import sklearn
 from sklearn.feature_selection import SelectKBest, f_regression, RFE
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.linear_model import LinearRegression
 
 def select_kbest(features, target_variable, k):
-    f_selector = SelectKBest(f_regression, k=k)
-    f_selector.fit(features, target_variable)
-    X_reduced = f_selector.transform(features)
-    f_support = f_selector.get_support()
-    f_feature = features.loc[:,f_support].columns.tolist()
-    return f_feature
+    kbest = SelectKBest(f_regression, k)
+    kbest.fit(features, target_variable)
+    return features.columns[kbest.get_support()]
 
-def rfe(features, target_variable, k):
+def select_rfe(features, target_variable, k):
     lm = LinearRegression()
     rfe = RFE(lm, k)
     rfe.fit(features, target_variable)
-    X_rfe = rfe.transform(features)
-    mask = rfe.support_
-    rfe_features = features.loc[:, mask].columns.tolist()
-    return rfe_features
+    return features.columns[rfe.support_]
