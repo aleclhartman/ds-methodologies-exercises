d220f7c3437ddf9e65b065e056aa415ea370df28
Alec Hartman <aleclhartman08@gmail.com>
2020-05-14 16:21:11 -0500

    completed simple average model

---
+++ time_series/model+evaluate.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+from datetime import datetime
+
+from sklearn.metrics import mean_squared_error
+
+from math import sqrt
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import statsmodels.api as sm
+from statsmodels.tsa.api import Holt
+
+import acquire as ac
+import prepare as pr
